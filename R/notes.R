e4b69ae7f2f650195d0b74ff9af966fcdb3a9565
PauloEduardoCardoso <pauloeducardoso@gmail.com>
2017-10-16 01:18:22 +0100

    note

--- R/notes.R
+++ R/notes.R
@@ -1,4 +1,5 @@
 #' Just to start inserting codes here
 #' I'd suggest us to work with functions from RSToolbox from Benjamin Leutner, particularly
 #' unsuperClass()
+#' https://stackoverflow.com/questions/1595848/configuring-git-over-ssh-to-login-once
 #' 
