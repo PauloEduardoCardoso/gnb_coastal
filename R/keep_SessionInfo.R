64e2dfd3ffaea5efb923b62fe95ae8ef169abf74
PauloEduardoCardoso <pauloeducardoso@gmail.com>
2017-10-19 00:05:04 +0100

    updates

---
+++ R/keep_SessionInfo.R
@@ -0,0 +1,17 @@
+# sessionInfo()
+# R version 3.4.2 (2017-09-28)
+# Platform: x86_64-w64-mingw32/x64 (64-bit)
+# Running under: Windows >= 8 x64 (build 9200)
+# 
+# Matrix products: default
+# 
+# locale:
+#   [1] LC_COLLATE=English_United Kingdom.1252  LC_CTYPE=English_United Kingdom.1252   
+# [3] LC_MONETARY=English_United Kingdom.1252 LC_NUMERIC=C                           
+# [5] LC_TIME=English_United Kingdom.1252    
+# 
+# attached base packages:
+#   [1] stats     graphics  grDevices utils     datasets  methods   base     
+# 
+# other attached packages:
+#   [1] RStoolbox_0.1.9 raster_2.5-8    sp_1.2-5       
--- R/notes.R
+++ R/notes.R
@@ -1,5 +1,16 @@
-#' Just to start inserting codes here
+#' Notes in English to avoid RStudio conflict with non UTF encoding.
+#' Just to start inserting code here
 #' I'd suggest us to work with functions from RSToolbox from Benjamin Leutner, particularly
 #' unsuperClass()
 #' https://stackoverflow.com/questions/1595848/configuring-git-over-ssh-to-login-once
+#' 
+#' We will explore Landsat Surface Reflectance Level-2 Science Data Products.
+#' Details here:
+#' https://landsat.usgs.gov/landsat-surface-reflectance-data-products
+#' 
+#' The best scene capturing exposed sediments is:
+#' ID:LC08_L1TP_204052_20170106_20170312_01_T1
+#' Acquisition Date:06-JAN-17
+#' Path:204
+#' Row:52
 #' 
