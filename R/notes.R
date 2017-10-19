9752a25d9198d413d20f7b50fbc2bd51f3093aa8
PauloEduardoCardoso <pauloeducardoso@gmail.com>
2017-10-19 13:43:04 +0100

    updates

--- R/notes.R
+++ R/notes.R
@@ -8,9 +8,17 @@
 #' Details here:
 #' https://landsat.usgs.gov/landsat-surface-reflectance-data-products
 #' 
+#' We will ideally work with Landsat scenes from Tier 1 collection
+#' more about Tier collections here:
+#' https://landsat.usgs.gov/what-are-landsat-collection-1-tiers
+#' 
+#' A Technical Guide for USGS products (focus on COllection 1)
+#' https://above.nasa.gov/pdfs/Landsat_Surface_Reflectance_ABoVE_21Apr2017.pdf
+#' 
 #' The best scene capturing exposed sediments is:
 #' ID:LC08_L1TP_204052_20170106_20170312_01_T1
 #' Acquisition Date:06-JAN-17
 #' Path:204
 #' Row:52
+#' 
 #' 
